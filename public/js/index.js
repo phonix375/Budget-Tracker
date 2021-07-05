844f50c94f774fc012c06f5b99126b5a0aab9f23
Alexy Kotliar <kotliar.alex@gmail.com>
2021-07-04 21:52:42 -0400

    added offline indexedDB database

--- public/js/index.js
+++ public/js/index.js
@@ -136,6 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
+    console.log(`this is what i'm sending: ` + transaction)
     saveRecord(transaction);
 
     // clear form
